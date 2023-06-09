2b028709a9b22a3338b9a01aa6f055c9c966565a
karthicbz <pkarthicbz@live.in>
2023-06-09 22:26:44 +0530

    add message into messages array

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,18 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/new', (req, res)=>{
-  res.render('newMessage');
-})
+  res.render('newMessage', {error:''});
+});
+
+router.post('/new', (req, res)=>{
+  if(req.body.userName && req.body.userMessage){
+    messages.push({text:req.body.userMessage, user:req.body.userName, added: new Date});
+    res.redirect('/');
+  }else{
+    res.render('newMessage', {error: 'name and message must not be empty'});
+    res.redirect('/new');
+  }
+  // res.redirect('http://localhost:3000');
+});
 
 module.exports = router;
