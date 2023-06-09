b5656347db1ba51b5a703a59d0dea3e60f653d65
karthicbz <pkarthicbz@live.in>
2023-06-09 21:38:20 +0530

    fetch and display messages

---
+++ Messages.js
@@ -0,0 +1,13 @@
+const messages = [{
+    text: 'Hi There',
+    user: 'Dinesh',
+    added: new Date(),
+},
+{
+    text: 'Hello World',
+    user: 'Divya',
+    added: new Date(),
+}
+]
+
+module.exports = messages;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const messages = require('../Messages');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {msg: messages});
 });
+
+router.get('/new', (req, res)=>{
+  res.send('New Message');
+})
 
 module.exports = router;
